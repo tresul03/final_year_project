fbf583cf1ec108b24568da7078862a2bbde55b5d
tresul03 <resul13@outlook.com>
2024-02-13 10:45:28 +0000

    add read_input_file method

--- src/semester2/week2/radiative_transfer_bnn.py
+++ src/semester2/week2/radiative_transfer_bnn.py
@@ -12,8 +12,8 @@ class RadiativeTransferBNN(nn.Module):
     def __init__(self, number_of_neurones, dropout_probablity, learning_rate):
         super(RadiativeTransferBNN, self).__init__()
 
-        is_available = torch.cuda.is_available()
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        cuda_available = torch.cuda.is_available()
+        device = torch.device("cuda:0" if cuda_available else "cpu")
 
         self.shared_layer = nn.Sequential(
             bnn.BayesLinear(  # input layer
@@ -33,7 +33,7 @@ class RadiativeTransferBNN(nn.Module):
             ),
             nn.ReLU(),
             nn.Dropout(dropout_probablity),
-            
+
             bnn.BayesLinear(  # 2nd hidden layer
                 prior_mu=0,
                 prior_sigma=0.1,
@@ -53,15 +53,56 @@ class RadiativeTransferBNN(nn.Module):
             )
         )
 
+        # loss function
         self.mse_loss = nn.MSELoss()
         self.kl_loss = bnn.BKLLoss(reduction='mean')
         self.kl_weight = 0.01
         self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
-        self.to(device)
+
+        self.to(device)  # move the model to the GPU if available
 
     def forward(self, x):
         shared = self.shared_layer(x)
         return self.output_layer(shared)
-    
 
-    # def initialise_model(self, learning_rate):
+    def read_input_file(
+            self,
+            filename: str,
+            filepath: str = '../../data/radiative_transfer/input/'
+            ):
+        """
+        Read the parameters from the input file.
+
+        Parameters:
+        - filename (str): Name of the input file.
+        - filepath (str): Path to the input file.
+        Default is '../../data/radiative_transfer/input/'.
+
+        Returns:
+        - table (dict): Dictionary containing the parameters.
+        """
+
+        lines = open(filepath+filename, 'r').readlines()
+
+        keys = []
+        values = []
+        for i in range(len(lines)):
+
+            line_i = lines[i]
+            line1 = line_i.split('\n')[0]
+            line2 = line1.split('#')[0]
+            line3 = line2.split('=')
+            line4 = []
+            for j in range(len(line3)):
+                line4.append(line3[j].strip(' '))
+
+            if len(line4) == 2:
+                keys.append(line4[0])
+                line5 = line4[1].split(', ')
+                line5 = np.array(line5).astype(float)
+                if len(line5) == 1 and line4[0]!='theta':
+                    line5 = line5[0]
+                values.append(line5)
+
+        table = dict(zip(keys, values))
+        return table
